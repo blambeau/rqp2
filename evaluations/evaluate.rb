b8b2ba96c92b81902c3b527760e856b5445ea285
Bernard Lambeau <blambeau@gmail.com>
2014-03-30 13:19:09 +0200

    Hardcoded evaluation for 2014

---
+++ evaluations/evaluate.rb
@@ -0,0 +1,130 @@
+require 'rqp2'
+
+class Tester
+
+  attr_reader :db, :my_submission, :tests, :dbmses
+
+  def initialize
+    @dbmses = Hash.new{|h,k| h[k] = RQP2::DBMS.for(k) }
+  end
+
+  def db
+    @db ||= RQP2::ALF_DATABASE.connection
+  end
+
+  def disconnect
+    @db.close if @db
+    @dbmses.each_value{|dbms| dbms.disconnect } if @dbmses
+  end
+
+  def my_submission
+    @my_submission ||= db.tuple_extract{
+      me            = restrict(students, name: 'Bernard Lambeau')
+      my_submission = matching(submissions, project(me, [:student]))
+    }[:submission]
+  end
+
+  def tests
+    sub = my_submission
+    @tests ||= db.relvar{
+      pred  = Alf::Predicate.eq(:submission1, sub)
+      pred &= Alf::Predicate.eq(:language1, :language2)
+      pred &= Alf::Predicate.neq(:language1, 'alf')
+      rename(restrict(pending_tests, pred), :language1 => :language)
+    }.to_a(order: [:language, :exemplar, :dataset])
+  end
+
+  def call
+    tests    = self.tests
+    language = nil
+    dataset  = nil
+    dbms     = nil
+    tests.each_with_index do |test, index|
+      test          = Tuple(test)
+      test_language = test.project([:language])
+      test_exemplar = test.project([:exemplar, :language])
+      test_dataset  = test.project([:exemplar, :dataset, :language])
+
+      # ensure the dbms is ok
+      unless language == test_language
+        language = test_language
+        dataset  = nil
+        dbms     = dbmses[language.language]
+
+        install_exemplar_on(test_exemplar, dbms)
+      end
+
+      # ensure the dataset is ok
+      unless dataset == test_dataset
+        dataset = test_dataset
+
+        install_dataset_on(dataset, dbms)
+      end
+
+      result = test.project([:answer1, :answer2, :exemplar, :dataset])
+                   .extend(execute_test_on(test, dbms))
+
+      if result[:outcome] == 'failure'
+        puts test.expression1
+        raise "Failure: " + result[:details]
+      elsif result[:outcome] == 'error'
+        puts test.expression2
+        raise "Error: " + result[:details]
+      end
+      db.relvar(:tests).insert(result)
+
+      puts "#{index}/#{tests.size}: #{result.inspect}"
+    end
+  ensure
+    disconnect
+  end
+
+  def execute_test_on(test, dbms)
+    # Take the one at left or fail
+    begin
+      rel1 = dbms.query(test.expression1)
+    rescue => ex
+      return { outcome: "failure", details: ex.message }
+    end
+
+    # Take the one at right or error
+    begin
+      rel2 = dbms.query(test.expression2)
+    rescue => ex
+      return { outcome: "parsing error", details: ex.message }
+    end
+
+    if not(rel1.empty? || rel2.empty?) && rel1.heading != rel2.heading
+      { outcome: "heading mismatch",
+        details: "    #{rel1.heading}\nvs. #{rel2.heading}" }
+    elsif rel1 != rel2
+      { outcome: "not equivalent",
+        details: Relation(expected: rel1, got: rel2).to_text }
+    else
+      { outcome: "equivalent",
+        details: 'good job!' }
+    end
+
+  rescue => ex
+    return { outcome: "error", details: ex.message }
+  end
+
+private
+
+  def install_exemplar_on(schema, dbms)
+    puts "Installing schema `#{schema}`"
+
+    s = db.tuple_extract{ restrict(schemas, schema) }
+    dbms.execute_ddl(s.formaldef) rescue nil
+  end
+
+  def install_dataset_on(dataset, dbms)
+    puts "Changing dataset to `#{dataset}`"
+
+    ds = db.tuple_extract{ restrict(datadefs, dataset) }
+    dbms.execute_dml(ds.formaldef)
+  end
+
+end
+
+Tester.new.call
---
+++ evaluations/reports.rb
@@ -0,0 +1,95 @@
+require 'rqp2'
+require 'wlang'
+
+class Dialect < WLang::Html
+
+  def floating(buf, fn)
+    value = evaluate(render(fn))
+    buf << ((value.to_i == value) ? value.to_i : value.to_f).to_s
+  end
+
+  def classes(buf, fn)
+    value   = evaluate(render(fn))
+    classes = []
+    classes << 'success' if value[:success]   == 1
+    classes << 'failure' if value[:success]   == 0
+    classes << 'ignored' if value[:accounted] == 0
+    buf << classes.join(' ')
+  end
+
+  tag '@', :classes
+  tag '.', :floating
+
+end
+
+class Reports
+
+  attr_reader :db
+
+  def initialize
+  end
+
+  def db
+    @db ||= RQP2::ALF_DATABASE.connection
+  end
+
+  def disconnect
+    @db.close if @db
+  end
+
+  def call
+    rv = db.relvar{
+      scoring = allbut(answer_scoring, [:reference])
+      answrs  = answers
+      puzzls  = allbut(puzzles, [:exemplar, :hints])
+      levels  = difficulty_levels
+      subsco  = extend(submission_scoring, score: ->(t){ t.score.to_f.round(2) })
+      subms   = join(allbut(submissions, :submitted_at), subsco)
+      stus    = students
+      bigjoin = join(join(stus, join(subms, join(answrs, join(puzzls, levels)))), scoring)
+
+      details = allbut(answer_scoring_details, [:reference, :exemplar])
+      details = group(details, [:answer], :details, allbut: true)
+      details = extend(details, details: ->(t){ t.details.to_a(order: [:dataset]) })
+      bigjoin = join(bigjoin, details)
+
+      bigjoin = group(bigjoin,
+        [ :submission, :student, :noma, :name, :evaluation, :score, :grade,
+          :puzzle, :difficulty, :level, :stars, :predicate, :description],
+        :answers, allbut: true)
+      bigjoin = extend(bigjoin,
+        answers: ->(t){
+          t.answers.to_a(order: [:position])
+        },
+        earned_stars: ->(t){
+          t.answers.sum(:earned_stars)
+        })
+      bigjoin = extend(bigjoin, status: ->(t){
+        if t.earned_stars.to_f == 0.0
+          "failure"
+        elsif t.earned_stars.to_f == t.stars.to_f
+          "success"
+        else
+          "partial"
+        end
+      })
+
+      bigjoin = group(bigjoin,
+        [ :submission, :student, :noma, :name, :evaluation, :score, :grade, ], :puzzles, allbut: true)
+      bigjoin = extend(bigjoin, puzzles: ->(t){ t.puzzles.to_a(order: [:level, :puzzle])})
+    }
+  ensure
+    disconnect
+  end
+
+end
+
+data = Reports.new.call #.restrict(submission: 'd2db6b40-9996-0131-54f9-3c07545ed162')
+data.each do |tuple|
+  tuple = Tuple(tuple)
+  puts "Reporting for #{tuple.name}"
+  target = tuple.name.gsub(/\s+/, '_') + ".html"
+  report = Dialect.render(Path.dir/'report.wlang', tuple)
+  (Path.dir/target).write report
+  `prince #{target}`
+end
